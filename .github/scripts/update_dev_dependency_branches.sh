c70711ed6f86e6d1f805e21c74c0d0fe1bbf26a3
Gerda Shank <gerda@dbtlabs.com>
2024-08-21 18:51:27 -0400

    Tweak update_dev_dependency_branches.sh to work for dbt-core

--- .github/scripts/update_dev_dependency_branches.sh
+++ .github/scripts/update_dev_dependency_branches.sh
@@ -6,7 +6,7 @@ dbt_adapters_branch=$1
 dbt_core_branch=$2
 dbt_common_branch=$3
 target_req_file="dev-requirements.txt"
-core_req_sed_pattern="s|dbt-core.git.*#egg=dbt-core|dbt-core.git@${dbt_core_branch}#egg=dbt-core|g"
+core_req_sed_pattern="s|dbt-core.git|dbt-core.git@${dbt_core_branch}|g"
 adapters_req_sed_pattern="s|dbt-adapters.git|dbt-adapters.git@${dbt_adapters_branch}|g"
 common_req_sed_pattern="s|dbt-common.git|dbt-common.git@${dbt_common_branch}|g"
 if [[ "$OSTYPE" == darwin* ]]; then
